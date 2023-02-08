62564bace78ae4b139bb4232a0b51f8d1048a460
JoseViewture <110395155+JoseViewture@users.noreply.github.com>
2023-02-08 21:54:58 +0000

    Add project files.

---
+++ Data/TransactionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BankTransactions.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BankTransactions.Data
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)   
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }    
+      
+    }
+}
